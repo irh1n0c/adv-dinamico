bdcab8f2dff6b94116fa348359e422a850d4f7eb
bitskoch <lhuaynasi@gmail.com>
2025-05-01 22:36:31 -0500

    delete server with railway

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: ['https://fismetventas.up.railway.app', 'http://localhost:5173'],
+  origin: ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
