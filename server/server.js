2ae21f9ac308eef6c64d791edbd90262865764ae
bitskoch <lhuaynasi@gmail.com>
2025-05-01 23:07:56 -0500

    update link cors solicitud localhost to vercel

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://adv-dinamico.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
